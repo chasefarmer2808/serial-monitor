53eb3cf208ae69e6d5aaf31688d923b75eb9b08b
Chase Farmer <chasefarmer2808@ufl.edu>
2017-07-15 19:57:58 -0400

    receive box read only

--- classes/App.py
+++ classes/App.py
@@ -1,6 +1,6 @@
 import Queue
 
-from Tkinter import Frame, Scrollbar, Text, BOTH, RIGHT, Y
+from Tkinter import Frame, Scrollbar, Text, BOTH, RIGHT, Y, DISABLED, NORMAL
 
 from SerialMonitor import SerialMonitor
 
@@ -12,7 +12,7 @@ class App(Frame):
 
         self.parent = parent
         self.scrollbar = Scrollbar(parent)
-        self.logbox = Text(parent, width=30, height=30, takefocus=0)
+        self.receive_box = Text(parent, width=30, height=30, takefocus=0)
 
         self.initUI()
         self.process_serial_input()
@@ -22,14 +22,17 @@ class App(Frame):
         self.parent.title("Serial Monitor")
         self.pack(fill=BOTH, expand=1)
         self.scrollbar.pack(side=RIGHT, fill=Y)
-        self.logbox.pack()
-        self.logbox.config(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.config(command=self.logbox.yview)
+        self.receive_box.pack()
+        self.receive_box.config(yscrollcommand=self.scrollbar.set, state=DISABLED)
+        self.scrollbar.config(command=self.receive_box.yview)
 
     def process_serial_input(self):
         while self.monitor.available():
             try:
-                self.logbox.insert('end', self.monitor.queue.get())
+                self.receive_box.config(state=NORMAL)
+                self.receive_box.insert('end', self.monitor.queue.get())
+                self.receive_box.config(state=DISABLED)
+
             except Queue.Empty:
                 pass
         self.after(100, self.process_serial_input)
