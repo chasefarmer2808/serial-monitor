5f6e93e484bf75bf87f8df6c28b4472c8fd7e159
Chase Farmer <chasefarmer2808@ufl.edu>
2017-07-23 18:10:10 -0400

    winforms app

---
+++ SerialMonitor/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.device = new System.IO.Ports.SerialPort(this.components);
+            this.portSelect = new System.Windows.Forms.ComboBox();
+            this.portSelectLabel = new System.Windows.Forms.Label();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.baudSelect = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // device
+            // 
+            this.device.PortName = "COM3";
+            // 
+            // portSelect
+            // 
+            this.portSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.portSelect.FormattingEnabled = true;
+            this.portSelect.Location = new System.Drawing.Point(32, 30);
+            this.portSelect.Name = "portSelect";
+            this.portSelect.Size = new System.Drawing.Size(149, 32);
+            this.portSelect.TabIndex = 0;
+            this.portSelect.SelectedIndexChanged += new System.EventHandler(this.portSelect_SelectedIndexChanged);
+            // 
+            // portSelectLabel
+            // 
+            this.portSelectLabel.AutoSize = true;
+            this.portSelectLabel.Location = new System.Drawing.Point(12, 2);
+            this.portSelectLabel.Name = "portSelectLabel";
+            this.portSelectLabel.Size = new System.Drawing.Size(107, 25);
+            this.portSelectLabel.TabIndex = 1;
+            this.portSelectLabel.Text = "Select Port";
+            // 
+            // scanButton
+            // 
+            this.scanButton.Location = new System.Drawing.Point(202, 26);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(111, 39);
+            this.scanButton.TabIndex = 2;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // baudSelect
+            // 
+            this.baudSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudSelect.FormattingEnabled = true;
+            this.baudSelect.Location = new System.Drawing.Point(382, 30);
+            this.baudSelect.Name = "baudSelect";
+            this.baudSelect.Size = new System.Drawing.Size(175, 32);
+            this.baudSelect.TabIndex = 3;
+            this.baudSelect.SelectedIndexChanged += new System.EventHandler(this.baudSelect_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Baud";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Enabled = false;
+            this.connectButton.Location = new System.Drawing.Point(630, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(135, 67);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 524);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.baudSelect);
+            this.Controls.Add(this.scanButton);
+            this.Controls.Add(this.portSelectLabel);
+            this.Controls.Add(this.portSelect);
+            this.Name = "Form1";
+            this.Text = "Ground Station";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort device;
+        private System.Windows.Forms.ComboBox portSelect;
+        private System.Windows.Forms.Label portSelectLabel;
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.ComboBox baudSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectButton;
+    }
+}
+
---
+++ SerialMonitor/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialMonitor
+{
+    public partial class Form1 : Form
+    {
+
+        public static readonly String[] BAUDS = { "9600", "11200" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            setAvailablePorts();
+            setBaudSelect();
+
+            device.DataReceived += new SerialDataReceivedEventHandler(serialPortDataRecieved);
+            //device.Open();
+        }
+
+        private void serialPortDataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            Invoke(new EventHandler(DisplaySerialData));
+        }
+
+        private void DisplaySerialData(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            portSelect.Items.AddRange(ports);
+        }
+
+        private void setBaudSelect()
+        {
+            baudSelect.Items.AddRange(BAUDS);
+        }
+
+        private void checkPortSettings()
+        {
+            bool portSelected = !string.IsNullOrWhiteSpace(portSelect.Text);
+            bool baudSelected = !string.IsNullOrWhiteSpace(baudSelect.Text);
+
+            if (portSelected && baudSelected)
+            {
+                connectButton.Enabled = true;
+            } else
+            {
+                connectButton.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            setAvailablePorts();
+        }
+
+        private void portSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkPortSettings();
+        }
+
+        private void baudSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkPortSettings();
+        }
+    }
+}
